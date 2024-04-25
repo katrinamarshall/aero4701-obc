79455949ae6995c930c3e63808646d2bfe4ad437
EmilyzeS <emily.sacks8@gmail.com>
2024-04-25 18:33:46 +1000

    8 pixels

--- for_pi/lidar_pi.py
+++ for_pi/lidar_pi.py
@@ -16,7 +16,7 @@ def gen_test_data(_num_pixels, _debris_freq, current_time, test_vel=False, empty
         # if testing velocity we just want one cluster of debris
         test_data = np.full((_num_pixels, _num_pixels), SENSOR_RANGE)
         if current_time < 1:
-            cluster_size1 = np.random.randint(100)
+            cluster_size1 = np.random.randint(5)
             clump1_center = np.random.randint(0, _num_pixels - cluster_size1, size=2)
             test_data[clump1_center[0]:clump1_center[0] + cluster_size1, clump1_center[1]:clump1_center[1] + cluster_size1] = 100
             # plt.imshow(test_data, cmap='viridis', interpolation= 'nearest')
