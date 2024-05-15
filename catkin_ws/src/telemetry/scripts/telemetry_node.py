16f8771f3998bf92dad68f78b36fc3f6cb8a75b0
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2024-05-15 10:29:37 +1000

    Testing ax.25

--- catkin_ws/src/telemetry/scripts/telemetry_node.py
+++ catkin_ws/src/telemetry/scripts/telemetry_node.py
@@ -7,7 +7,15 @@ from trancsivers import AX25UIFrame
 
 # Callback to receive downlink data instructions
 def downlink_data_callback(data: String, transceiver: Transceiver):
-    transceiver.send_deal(data.data)
+    info = data.data  # Information field to be filled by the user
+    ssid_type = 0b1111  # Science Data
+
+    ax25_frame = AX25UIFrame(info, ssid_type)
+    frame = ax25_frame.create_frame()
+
+    transceiver.send_deal(frame)
+
+    # transceiver.send_deal(data.data)
     
 
 # Function to receive data and publish to the topic
