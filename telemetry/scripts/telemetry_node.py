702c92c75bbf7ae888e2ad641fe28e30473029c5
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2024-05-15 21:55:51 +1000

    Still testing bytes

--- telemetry/scripts/telemetry_node.py
+++ telemetry/scripts/telemetry_node.py
@@ -7,8 +7,8 @@ from AX25UI import AX25UIFrame
 
 # Callback to receive downlink data instructions
 def downlink_data_callback(data: String, transceiver: Transceiver):
-    info = data.data  # Information field to be filled by the user
-    ssid_type = int(data[:4]).to_bytes()
+    info = data.data  
+    ssid_type = int(info[:4]).to_bytes()
     print(f"SSID type: {ssid_type}")
     # ssid_type = 0  # Science Data
 
