963a349dd7fcde145d68c7f653768605abb1218b
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2024-05-16 11:30:41 +1000

    testing wod data

--- telemetry/scripts/telemetry_node.py
+++ telemetry/scripts/telemetry_node.py
@@ -28,6 +28,9 @@ class Telemetry:
         # Subscriber for statellite pose data
         rospy.Subscriber('/satellite_pose_data', satellite_pose, self.satellite_pose_data_callback)
 
+        # Subscriber for WOD data
+        rospy.Subscriber('/wod_data', WOD, self.wod_data_callback)
+
     def downlink_data_callback(self, data):
         """Sends a miscellaneous message to be sent by the transceiver"""
         info = data.data  
@@ -86,6 +89,34 @@ class Telemetry:
         # Send data
         self.transceiver.send_deal(frame)
 
+
+    def wod_data_callback(self, data):
+        """Packs and sends WOD data"""
+        # Pack the time field (32 bits)
+        time_field = struct.pack('<I', data.packet_time_size)
+
+        # Pack each data set (57 bits)
+        datasets = b''.join(pack_wod_dataset(dataset) for dataset in data.datasets)
+
+        # Ensure the length of datasets is correct (32 data sets of 57 bits each = 1824 bits)
+        expected_length = 32 * 57 // 8
+        if len(datasets) < expected_length:
+            datasets = datasets.ljust(expected_length, b'\x00') # Pads with zeroes
+
+        # Combine time field and datasets
+        info = time_field + datasets
+
+        print(f"Packed WOD Data: {info.hex()} (Length: {len(info)} bytes)")
+        ssid_type = 0b1110  # WOD data type
+
+        # Create an ax.25 UI frame
+        ax25_frame = AX25UIFrame(info, ssid_type)
+        frame = ax25_frame.create_frame()
+
+        # Send data
+        self.transceiver.send_deal(frame)
+
+
     def run(self):
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
@@ -94,6 +125,23 @@ class Telemetry:
                 rospy.loginfo(f"Received and publishing: component={command.component}, component_id={command.component_id}, command={command.command}")
                 self.uplink_publisher.publish(command)
             rate.sleep()
+
+
+
+# Helper functions ----------------------------------------------------------------
+def pack_wod_dataset(dataset):
+    """Pack a single WOD data set into binary format."""
+    return struct.pack('8B', 
+        dataset.satellite_mode,
+        dataset.battery_voltage,
+        dataset.battery_current,
+        dataset.regulated_bus_current_3v3,
+        dataset.regulated_bus_current_5v,
+        dataset.temperature_comm,
+        dataset.temperature_eps,
+        dataset.temperature_battery
+    )
+
 
 if __name__ == '__main__':
     telemetry = Telemetry()
