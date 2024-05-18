f6f619f19a5c8b86b16b2531b4eb89d47fcd1873
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2024-05-18 22:42:43 +1000

    shits fucked

--- telemetry/scripts/telemetry_node.py
+++ telemetry/scripts/telemetry_node.py
@@ -116,13 +116,7 @@ class Telemetry:
         self.message_queue.put(second_frame)
 
     def run(self):
-        rate = rospy.Rate(1)
-        while not rospy.is_shutdown():
-            command = self.transceiver.receive_data()
-            if command:
-                rospy.loginfo(f"Received and publishing: component={command.component}, component_id={command.component_id}, command={command.command}")
-                self.uplink_publisher.publish(command)
-            rate.sleep()
+        rospy.spin()
 
 # Helper functions ----------------------------------------------------------------
 def convert_voltage(voltage):
