7e109fc4e4d8e92940174ee80f6f0213dca6a8f4
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2024-05-22 20:04:28 +1000

    empty frame for debugging

--- telemetry/scripts/telemetry_node.py
+++ telemetry/scripts/telemetry_node.py
@@ -112,15 +112,16 @@ class Telemetry:
                 lidar_data = struct.pack('64H', *distance_array)
                 # lidar_data = struct.pack(f'{len(distance_array)}H', *distance_array)
 
+                empty = struct.pack('B', 0)
                 # Create frame
-                frame_info = lidar_num + lidar_data + lidar_num
+                frame_info = lidar_num + lidar_data + empty
                 # print(f"Frame size: {struct.calcsize(frame_info)}")
                 print(f"Frame size: {len(frame_info)} bytes")
                 ssid_type = 0b1100  # Raw lidar data type
                 frame = AX25UIFrame(frame_info, ssid_type)
 
                 # Put message into frame
-                print(f"Putting the raw_lidar frame into the queue: {lidar_data}")
+                print(f"Putting the raw_lidar frame into the queue: {frame_info}")
                 self.message_queue.put(frame.create_frame())
 
 
