de12b06177d01a318f193b7805f0bf83d2dc7866
Ming <83496544+mingz0o@users.noreply.github.com>
2024-05-22 21:24:02 +1000

    Update temp_monitor.py
    4 THERMISTOR CONFIGURATION YOLO

--- temperature_monitor/scripts/temp_monitor.py
+++ temperature_monitor/scripts/temp_monitor.py
@@ -7,7 +7,6 @@ from time import strftime
 
 import rospy
 from std_msgs.msg import String
-
 import RPi.GPIO as GPIO
 #ROS SETUP
 # Initialize the node
@@ -26,7 +25,7 @@ spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1350000  # Ensure under 1.35 MHz
 
-#Constants
+# Constants
 v_in = 3.3  # Input voltage
 R_ref = 10000  # Fixed reference resistor (10k ohm)
 
@@ -42,19 +41,20 @@ def readADC(channelNum):
 def getTemp(adc_channel):
     raw_data = readADC(adc_channel)  # Read the ADC channel to get value
     voltage = (raw_data * v_in) / 1024  # Convert ADC raw data to voltage
-    
-    if voltage < 10:
-        rospy.logwarn(f"Channel {adc_channel}: Voltage zero detected, possibly unconnected. ")
-        return -9
-    
-    #Voltage divider formula
-    R_thermistor = R_ref * ((v_in / voltage) - 1)  #
 
-    # Steinhart-Hart coefficients for 10k NTC thermistor
-    a = 0.001125308852122
-    b = 0.000234711863267
-    c = 0.000000085663516
+    if voltage < 0.01:
+        rospy.logwarn(f"Channel {adc_channel}: Voltage zero detected, possibly unconnected.")
+        return 0  # Hardcoded temperature for unconnected or faulty channels
 
+    R_thermistor = R_ref * ((v_in / voltage) - 1)  # Calculate thermistor resistance using voltage divider formula
+
+    # Steinhart-Hart coefficients for a typical 10k NTC thermistor
+    a = 0.0010295
+    b = 0.000239255
+    c = 0.0000001558
+    # a = 0.001129148
+    # b = 0.000234125
+    # c = 0.0000000876741
     ln_r = math.log(R_thermistor)  # Ensure R_thermistor > zero before taking log
     t1 = b * ln_r
     t2 = c * ln_r ** 3
@@ -74,13 +74,12 @@ def getPiTemp():
     return cpu_temp
 
 while not rospy.is_shutdown():
-    temps = [getTemp(i) for i in range(3)]
+    temps = [getTemp(i) for i in range(4)]
     pi_temp = getPiTemp()
-    # current_time = strftime("%H:%M")
-    message = f"{','.join(f'{t:.2f}' for t in temps)},{pi_temp:.2f}"
-    # message = f"{current_time},{','.join(f'{t:.2f}' for t in temps)},{pi_temp:.2f}"
+    current_time = strftime("%H:%M")
+    message = f"{current_time},{','.join(f'{t:.2f}' for t in temps)},{pi_temp:.2f}"
     rospy.loginfo(message)  # Log msg to ROS
     pub.publish(message)  # Publish msg
-    # file.write(f"{message}\n")
-    # file.flush()
+    #file.write(f"{message}\n")
+    #file.flush()
     rate.sleep()
