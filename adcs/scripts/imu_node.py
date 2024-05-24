76be14348e7c85b71522f433bc2e7e26a3ee2097
katrinamarshall <katrinaemarshall2@gmail.com>
2024-05-24 12:31:04 +1000

    imu working with quaternions

--- adcs/scripts/imu_node.py
+++ adcs/scripts/imu_node.py
@@ -12,10 +12,6 @@ from adcs.msg import imu_data_packet
 class IMU:
     def __init__(self):
 
-        # Initialise timer to take readings
-        self.imu_active = False
-        self.bno055_reader = rospy.Timer(rospy.Duration(1.0/10.0), self.get_imu_data)
-
         # Publishers
         self.pub = rospy.Publisher('/imu_data', imu_data_packet, queue_size=10)
         
@@ -25,6 +21,11 @@ class IMU:
         # Initialise imu
         self.i2c = board.I2C()
         self.bno055 = adafruit_bno055.BNO055_I2C(self.i2c)
+        self.bno055.mode = adafruit_bno055.NDOF_MODE
+
+        # Initialise timer to take readings
+        self.imu_active = True
+        self.bno055_reader = rospy.Timer(rospy.Duration(1.0/10.0), self.get_imu_data)
 
     # Callback for state changes
     def callback_state(self, state):
@@ -39,10 +40,10 @@ class IMU:
         if self.imu_active:
             msg = imu_data_packet()
 
-            msg.acceleration = self.bno055.acceleration # Accelerometer (m/s^2)
-            msg.gyro = self.bno055.gyro # Gyroscope (rad/sec)
-            msg.magnetometer = self.bno055.magnetic # Magnetometer (microteslas)
-            msg.temperature = self.bno055.temperature # Temperature (degrees C)
+            # msg.acceleration = self.bno055.acceleration # Accelerometer (m/s^2)
+            # msg.gyro = self.bno055.gyro # Gyroscope (rad/sec)
+            # msg.magnetometer = self.bno055.magnetic # Magnetometer (microteslas)
+            msg.orientation = self.bno055.quaternion # Temperature (degrees C)
 
             self.pub.publish(msg)
 
